36b3d800bb3c8a525a55c3e5ffd7c48658f46f98
Levi Oliveira dos Santos <leviolisan@gmail.com>
2026-03-16 16:18:46 -0300

    Refactor file handling and random word selection
    Fix file reading and random selection of words.

--- main.py
+++ main.py
@@ -1,84 +1,83 @@
-with open("palavras.txt", "r") as arquivo:
-
-    palavras = []
-    for line in arquivo:
-        palavra = line.strip()
-        palavras.append(palavra)
-
-    print(f"Digite um numero: ", end="")
-    try:
-        idx = int(input())
-    except:
-        print("Precisa ser um numero, jogador!")
+try:
+    with open("palavras.txt", "r") as arquivo:
+        palavras = arquivo.readlines()
+except FileNotFoundError:
+    print("Erro: o arquivo palavras.txt não foi encontrado.")
+    exit()
     
-    if len(palavras) == 0:
-        print(f"Erro. Nao ha palavras no banco de dados.")
-        exit(0)
+palavras = []
+for line in arquivo:
+    palavra = line.strip()
+    palavras.append(palavra)
+
+from random import radint
+if len(palavras) == 0:
+    print(f"Erro. Nao ha palavras no banco de dados.")
+    exit(0)
         
-    palavra = palavras[idx % len(palavras)]
-    lenght = len(palavra)
+palavra = palavras[radint(0, len(palavras)-1]
+lenght = len(palavra)
     
-    letras_tentadas = []
-    letras_da_palavra = []
+letras_tentadas = []
+letras_da_palavra = []
 
-    for letra in palavra:
-        letras_da_palavra.append(letra)
-
-    tentativas = 7
-
-    def print_jogo():
-        print("Palavra:", end="")
-        for i in range(lenght):
-            if palavra[i] in letras_tentadas and palavra[i] in letras_da_palavra:
-                print(f" {palavra[i]}", end='')
-            else:
-                print(" _", end='')
-
-    def print_letras_tentadas():
-        print("Letras tentadas:", end="")
-
-        if len(letras_tentadas) == 0:
-            print(" Nenhuma.", end="")
-            return
-                
-        for letra in letras_tentadas:
-            print(f" {letra}", end="")
-
-    def win():
-        for letra in letras_da_palavra:
-            if not letra in letras_tentadas:
-                return False
-        return True
+for letra in palavra:
+    letras_da_palavra.append(letra)
     
-    print("\nBem vindo ao jogo da forca!\n")
+tentativas = 7
 
-    while(tentativas > 0):
-        print_jogo()
-        print(f"\nTentativas restantes: {tentativas}")
-        print_letras_tentadas()
-
-        print("\n\nDigite uma letra maiuscula e sem acento: ")
-        
-        letra = input()
-        if len(letra) > 1 or not letra.isupper():
-            print(f"\nTem que ser letra e maiuscula, jogador!\n")
-            continue
-
-        if letra in letras_tentadas:
-            print("\nEssa letra ja foi tentada anteriomente, jogador. - to te tando uma colher de cha, nao vou diminuir teus pontos :)\n")
-            continue
-        
-        letras_tentadas.append(letra)
-        if letra in letras_da_palavra:
-            print(f"\nBoa! A letra '{letra}' está na palavra.\n")
+def print_jogo():
+    print("Palavra:", end="")
+    for i in range(lenght):
+        if palavra[i] in letras_tentadas and palavra[i] in letras_da_palavra:
+            print(f" {palavra[i]}", end='')
         else:
-            print(f"\nUma pena! A letra '{letra}' nao está na palavra.\n")
-            tentativas -= 1
+            print(" _", end='')
 
-        if win():
-            print(f"Parabens, voce acertou! A palavra era {palavra}.\n")
-            break
+def print_letras_tentadas():
+    print("Letras tentadas:", end="")
+    if len(letras_tentadas) == 0:
+        print(" Nenhuma.", end="")
+        return
+                
+    for letra in letras_tentadas:
+        print(f" {letra}", end="")
+        
+def win():
+    for letra in letras_da_palavra:
+        if not letra in letras_tentadas:
+            return False
+    return True
+    
+print("\nBem vindo ao jogo da forca!\n")
 
-    if not win():
-        print(f"\nFim de jogo, a palvra era {palavra}.\n")
+while(tentativas > 0):
+    print_jogo()
+    print(f"\nTentativas restantes: {tentativas}")
+    print_letras_tentadas()
 
+    print("\n\nDigite uma letra maiuscula e sem acento: ")
+    letra = input()
+
+    if len(letra) > 1 or not letra.isupper():
+        print(f"\nTem que ser letra e maiuscula, jogador!\n")
+        continue
+
+    if letra in letras_tentadas:
+        print("\nEssa letra ja foi tentada anteriomente, jogador. - to te tando uma colher de cha, nao vou diminuir teus pontos :)\n")
+        continue
+        
+    letras_tentadas.append(letra)
+
+    if letra in letras_da_palavra:
+        print(f"\nBoa! A letra '{letra}' está na palavra.\n")
+    else:
+        print(f"\nUma pena! A letra '{letra}' nao está na palavra.\n")
+        tentativas -= 1
+
+    if win():
+        print(f"Parabens, voce acertou! A palavra era {palavra}.\n")
+        break
+
+if not win():
+    print(f"\nFim de jogo, a palvra era {palavra}.\n")
